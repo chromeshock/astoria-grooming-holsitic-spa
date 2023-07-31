87c7aaa8f5f5da3189a3ecb6a469ba889f732413
chromeshock <cruzhector124@gmail.com>
2023-07-30 22:35:45 -0400

    Updated gallery content

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,41 +1,35 @@
-import React from 'react';
-import ImageGallery from 'react-image-gallery';
+import React, { useState } from 'react';
 import 'react-image-gallery/styles/css/image-gallery.css';
-//import gallery1 from './images/gallery1.jpg';
-//import gallery2 from './images/gallery2.jpg';
-//import demoya1 from './images/demoya1.jpg';
-//import demoya2 from './images/demoya2.jpg';
-//import gallery7 from './images/gallery7.jpg';
+import pet1 from '../images/pet1.jpg';
+import pet2 from '../images/pet2.jpg';
+import pet3 from '../images/pet3.jpg';
+import pet4 from '../images/pet4.jpg';
+import pet5 from '../images/pet5.jpg';
+import pet6 from '../images/pet6.jpg';
+import pet7 from '../images/pet7.jpg';
+import pet8 from '../images/pet8.jpg';
+import pet9 from '../images/pet9.jpg';
+import pet10 from '../images/pet10.jpg';
+import pet11 from '../images/pet11.jpg';
+import pet12 from '../images/pet12.jpg';
 
 function Gallery() {
-  const images = [
-    {
-     // original: gallery1,
-     // thumbnail: gallery1,
-    },
-    {
-      //original: gallery2,
-      //thumbnail: gallery2,
-    },
-    {
-     //original: demoya1,
-     //thumbnail: demoya1,
-    },
-    {
-      //original: demoya2,
-      //thumbnail: demoya2,
-    },
-    {
-     // original: gallery7,
-     // thumbnail: gallery7,
-    },
-   ];
-  
+  const images = [ pet1, pet2, pet3, pet4, pet5, pet6, pet7, pet8, pet9, pet10, pet11, pet12];
+  const [hoveredIndex, setHoveredIndex] = useState(null); // keep track of which image is currently being hovered over
     return (
-      <div>
-        <h2>Gallery</h2>
-        <div className='gallery-img'>
-          <ImageGallery items={images} />
+      <div className='gallery'>
+        <h1>Gallery</h1>
+        <div className='image-grid'>
+          {images.map((image, index) => (
+            <div 
+              key = {index} 
+                className = {`image-item ${hoveredIndex === index ? '' : 'dim'} `} // add the 'dim' class to all images that aren't currently being hovered over
+                onMouseOver = {() => setHoveredIndex(index)}  // when this image is hovered over, update hoveredIndex to this image's index
+                onMouseOut = {() => setHoveredIndex(null)} // when this image is no longer hovered over
+                >
+              <img src = {image} alt = {`Pet ${index + 1} `} />
+        </div>
+          ))}
         </div>
       </div>
     );
