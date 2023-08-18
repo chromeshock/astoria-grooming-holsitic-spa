da5f029a4699b17df32e3ef0d4a4c006191b3927
Hector Cruz <108369068+chromeshock@users.noreply.github.com>
2023-08-18 06:14:00 -0400

    animations

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -1,21 +1,40 @@
-  import React from 'react';
-  import Card from '@mui/material/Card';
-  import CardContent from '@mui/material/CardContent';
-  import Typography from '@mui/material/Typography';
+import React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
 
-  function ServiceCard({ service }) {
-    return (
-      <Card className='service-card' variant="outlined" style={{ marginBottom: '16px' }}>
-        <CardContent>
-          <Typography variant="h5" component="div" >
-            {service.name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" >
-            {service.description}
-          </Typography>
-        </CardContent>
-      </Card>
-    );
-  }
+function ServiceCard({ service }) {
+  return (
+    <div className="card-container">
+      <div className="card-flip">
+        <div className="card-front">
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                {service.name}
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                {service.description}
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+        <div className="card-back">
+          <Card variant="outlined">
+            <CardContent>
+              <Typography variant="h5" component="div">
+                More Details
+              </Typography>
+              <Typography variant="body2" color="textSecondary">
+                {service.details}
+              </Typography>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-  export default ServiceCard;
+
+export default ServiceCard;
