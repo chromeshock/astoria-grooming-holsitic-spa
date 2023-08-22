1a8f42c9f3860d54cd9d14aa56a1694323fdcc89
Hector Cruz <108369068+chromeshock@users.noreply.github.com>
2023-08-22 19:52:40 -0400

    services layout change

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -4,49 +4,37 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { useState } from'react';
 
-function ServiceCard({ service }) {
-  const [isFlipped, setFlipped] = useState(false);
 
-  const handleFlip = () => {
-    setFlipped(!isFlipped);
+function ServiceCard({ service }) {
+  const [isExpanded, setExpanded] = useState(false);
+
+  const handleExpand = () => {
+    setExpanded(!isExpanded);
   };
 
   return (
-    <div className="card-container" onClick= {handleFlip}>
-      <div className={`card-flip ${isFlipped ? 'flipped' : ''}`}>
-        <div className="card-front">
-          <div className="service-card">
-            <Card variant="outlined">
-              <CardContent>
-                <Typography variant="h5" component="div">
-                  <p>{service.name}</p>
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                 <p>{service.price}</p>
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                 <p>{service.duration}</p>
-                </Typography>
-                <Typography variant="body2" component="div">
-                 <p>{service.details}</p> 
-                </Typography>
-              </CardContent>
-            </Card>
-          </div>
-        </div> {/* This closing div tag was missing */}
-        <div className="card-back">
-          <Card variant="outlined">
-            <CardContent>
-              <Typography variant="h5" component="div">
-               <p>More Details</p> 
-              </Typography>
-              <Typography variant="body2" color="textSecondary">
-                <p>{service.descriptions}</p>
-              </Typography>
-            </CardContent>
-          </Card>
-        </div>
-      </div>
+    <div className={`card-container ${isExpanded ? 'expanded' : ''}`} onClick={handleExpand}>
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h5" component="div">
+            <p>{service.name}</p>
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+           <p>{service.price}</p>
+          </Typography>
+          <Typography variant="body2" color="textSecondary">
+           <p>{service.duration}</p>
+          </Typography>
+          <Typography variant="body2" component="div">
+           <p>{service.details}</p>
+          </Typography>
+          {isExpanded && (
+            <Typography variant="body2" color="textSecondary">
+              <p>{service.descriptions}</p>
+            </Typography>
+          )}
+        </CardContent>
+      </Card>
     </div>
   );
 }
