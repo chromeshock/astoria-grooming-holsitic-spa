ea1b9043108a37005d76ef1d805228d1394215a8
Hector Cruz <108369068+chromeshock@users.noreply.github.com>
2023-08-27 22:45:08 -0400

    revised layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,24 @@ import { Link } from 'react-router-dom';
 
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = React.useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+  
   return (
     <nav>
-      <ul>
+      <div className="hamburger-icon" onClick={toggleMenu}>
+        <div className="bar"></div>
+        <div className="bar"></div>
+        <div className="bar"></div>
+      </div>
+      <ul className={showMenu ? 'show' : ''}>
         <li><Link to ="/">Home</Link></li>
         <li><Link to ="/about">About</Link></li>
         <li><Link to ="/contact">Contact</Link></li>
         <li><Link to ="/gallery">Gallery</Link></li>
         <li><Link to ="/services">Services</Link></li>
-        <li><Link to ="/BookingCalendar">Schedule</Link></li>
         <a href="https://grooming-spa-2275-27-street-astoria-11105.square.site/" 
                     target="_blank"
                     rel="noopener noreferrer"
--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -3,36 +3,37 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { useState } from'react';
-
+import Fade from '@mui/material/Fade';
 
 function ServiceCard({ service }) {
   const [isExpanded, setExpanded] = useState(false);
 
   const handleExpand = () => {
     setExpanded(!isExpanded);
+    console.log(isExpanded);
   };
 
   return (
     <div className={`card-container ${isExpanded ? 'expanded' : ''}`} onClick={handleExpand}>
       <Card variant="outlined">
         <CardContent>
-          <Typography variant="h5" component="div">
+          <Typography variant="h5" component="div"  sx={{ fontSize: '2.2rem' }}> 
             <p>{service.name}</p>
           </Typography>
-          <Typography variant="body2" color="textSecondary">
+          <Typography variant="body2" color="textSecondary" sx={{ fontSize: '2rem' }}>
            <p>{service.price}</p>
           </Typography>
-          <Typography variant="body2" color="textSecondary">
+          <Typography variant="body2" color="textSecondary" sx={{ fontSize: '2rem' }}>
            <p>{service.duration}</p>
           </Typography>
-          <Typography variant="body2" component="div">
+          <Typography variant="body2" component="div" sx={{ fontSize: '1.3rem' }}>
            <p>{service.details}</p>
           </Typography>
-          {isExpanded && (
+           <Fade in={isExpanded} timeout={500}>
             <Typography variant="body2" color="textSecondary">
               <p className='txt-formatted'>{service.descriptions}</p>
             </Typography>
-          )}
+          </Fade>
         </CardContent>
       </Card>
     </div>
