66ebb4ea6f2406d84aeda4993c26f831b0d76f13
Hector Cruz <108369068+chromeshock@users.noreply.github.com>
2023-08-28 13:20:01 -0400

    homepage revised

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,11 +21,11 @@ const Navbar = () => {
         <li><Link to ="/contact">Contact</Link></li>
         <li><Link to ="/gallery">Gallery</Link></li>
         <li><Link to ="/services">Services</Link></li>
-        <a href="https://grooming-spa-2275-27-street-astoria-11105.square.site/" 
+        <li><a href="https://grooming-spa-2275-27-street-astoria-11105.square.site/" 
                     target="_blank"
                     rel="noopener noreferrer"
                     onClick={() => { alert('You will be redirected to an external site for processing...'); }}
-                >Book Now</a>
+                >Book Now</a></li>
       </ul>
     </nav>
   );
