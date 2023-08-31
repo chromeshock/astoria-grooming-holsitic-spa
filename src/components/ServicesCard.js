53ece2fa4a5504c27ebc355fd42021401cd9936c
Hector Cruz <108369068+chromeshock@users.noreply.github.com>
2023-08-31 12:30:28 -0400

    revised layout

--- src/components/ServicesCard.js
+++ src/components/ServicesCard.js
@@ -17,23 +17,21 @@ function ServiceCard({ service }) {
     <div className={`card-container ${isExpanded ? 'expanded' : ''}`} onClick={handleExpand}>
       <Card variant="outlined">
         <CardContent>
-          <Typography variant="h5" component="div"  sx={{ fontSize: '2.2rem' }}> 
+          <Typography variant="h5" component="div"  sx={{ fontSize: '2.4rem' }}> 
             <p>{service.name}</p>
           </Typography>
-          <Typography variant="body2" color="textSecondary" sx={{ fontSize: '2rem' }}>
-           <p>{service.price}</p>
+          <Typography variant="body2" color="textSecondary" sx={{ fontSize: '1.5rem' }}>
+           <p>{service.price} - {service.duration}</p>
           </Typography>
-          <Typography variant="body2" color="textSecondary" sx={{ fontSize: '2rem' }}>
-           <p>{service.duration}</p>
-          </Typography>
-          <Typography variant="body2" component="div" sx={{ fontSize: '1.3rem' }}>
-           <p>{service.details}</p>
-          </Typography>
+         
            <Fade in={isExpanded} timeout={500}>
-            <Typography variant="body2" color="textSecondary">
+            <Typography variant="body2" color="textSecondary" >
               <p className='txt-formatted'>{service.descriptions}</p>
             </Typography>
           </Fade>
+           <Typography variant="body2" component="div" className='description-text' sx={{ fontSize: '1.5rem' }}>
+           <p>{service.details}</p>
+          </Typography>
         </CardContent>
       </Card>
     </div>
