ad5b3b078177947adf7a265eb9b6f428bbcc81ad
Hector Cruz <108369068+chromeshock@users.noreply.github.com>
2023-09-20 15:36:32 -0400

    66% completed  services card

--- src/components/i18n.js
+++ src/components/i18n.js
@@ -9,73 +9,124 @@ const resources = {
   en: {
     //keys to be translated
     translation: {
-      welcome: "Welcome to our Astoria Grooming Holistic Pet Spa",
-      CleanEars: "Clean Ears",
-      duration: "MIN",
-      details: "No further details",
-      "Cats kitty lion cut":`This service includes body shaving, mane maintenance, and grooming of the feet and a portion of the tail. We also clip nails, clean ears, and offer a dry bath featuring a specialized shampoo with micro-crystals to cleanse your cat's skin. Additional charges may apply if the cat has severely matted fur. We do not shave cats over 14 years old or underweight cats due to the fragility of their skin and the potential for injury from the clippers.
+      welcome: 'Welcome to our Astoria Grooming Holistic Pet Spa',
+      //1st row
+        'Cat Claw Trim': 'Cat Claw Trim',
+        'Nail Clipper for dogs': 'Nail Clipper for dogs',
+        'CleanEars': 'Clean Ears',
+        'Paws Clean': 'Paws Clean',
 
-      For first-time clients, we require an initial evaluation to assess the cat's behavior. If the cat exhibits signs of extreme stress or aggression, we may be unable to provide the full range of services. However, you will only be charged for the services we are able to complete. For example, if we only manage to clip the nails, you will only be charged for that service.
+      //2nd row
+        'Extra Desheading': 'Extra Desheading',
+        'Face Trim': 'Face Trim',
+        'Cats kitty lion cut': 'Cats kitty lion cut',
+        'Cat Services': 'Cat Services',
 
-      We do not administer any sedatives or relaxants, as that goes against our policy. All grooming sessions are conducted in separate rooms to minimize stress, particularly for cats. A Rabies Certificate is required for all pets.`,
+      //3rd row
+        'Cats Long Hair': 'Cats Long Hair',
+        'Cat classic for Kittens': 'Cat classic for Kittens',
+        'Extra Dematting': 'Extra Dematting',
+        'Medium double coat': 'Medium double coat',
 
-      "fullSpaDogs": `For admission to our facility, a certificate confirming vaccinations for rabies, distemper, and bordetella is required. Your dog will enjoy a relaxing bath featuring a range of shampoos, including oatmeal, tearless, and hypoallergenic options, along with a blueberry facial and conditioner. We also trim nails, clean ears, and cut hair. A finishing touch of shining spray is applied. Additional fees may apply for dogs with severely matted fur.`,
-    
-      rabiesCert: `Required: Certificate of up to date vaccines. Rabies and bordetella`,
+      //4th row
+        'Full spa extra small dogs': 'Full spa extra small dogs',
+        'Full groom spa small dogs': 'Full groom spa small dogs',
+        'Full groom spa medium dogs': 'Full groom spa medium dogs',
+        'Full groom spa large dogs': 'Full groom spa large dogs',
       
-      catsLongHair: `Requirements: A Rabies Certificate is mandatory for all cats. First-time feline clients will need an evaluation on the same day as their scheduled appointment. We assess each cat's behavior, and unfortunately,  we cannot accommodate cats that are aggressive or highly stressed. If the cat only allows us to clip its nails, you will only be charged for that service. No charges will apply if we are unable to perform any services.   Additional fees may be incurred for cats with severely matted fur.`,
-      
-      xtraDmat: "This service is a complementary service for dogs Double coats.",
-      
-      boarding24: "Overnight 24 hours",
-      
-      catServices: "Trim nails, clean ears, sanitary area.  Required: Certificate of  up to date vaccines. "
-      // ... more keys
+      //5th row
+        'Full spa extra large dogs': 'Full spa extra large dogs',
+        'Full spa Double coat Large': 'Full spa Double coat Large',
+        'Dogs basic spa extra small': 'Dogs basic spa extra small',
+        'Dogs basic spa small short hair': 'Dogs basic spa small short hair',
+
+      //6th row
+         'Dogs basic spa medium short hair': 'Dogs basic spa medium short hair',
+         'Dogs basic spa large short hair': 'Dogs basic spa large short hair',
+         'Dogs Basic spa xl dogs': 'Dogs Basic spa xl dogs',
+         'Dogs Basic small spa double coat': 'Dogs Basic small spa double coat',
+
+      //7th row
+        'Dogs Basic small spa long hair': 'Dogs Basic small spa long hair',
+        'Dogs Basic extra small spa long hair': 'Dogs Basic extra small spa long hair',
+        'Dogs Basic extra small spa double coat spa': 'Dogs Basic extra small spa double coat spa',
+        'Dogs Basic extra large dog standard poodle and poddle mix spa': 'Dogs Basic extra large dog standard poodle and  poddle mix spa',
+
+      //8th row
+        'Dogs Basic Extra large long hair': 'Dogs Basic Extra large long hair',
+        'Dogs Basic Extra large coat double coat spa': 'Dogs Basic Extra large coat double coat spa',
+         'Cocker Spaniel, Cavalier Charles Kings, Cookapoo Or similare mixes': 'Cocker Spaniel, Cavalier Charles Kings, Cookapoo Or simmilare mixes',
+
+      //9th row
+      'Boarding Services X 24 Hours': 'Boarding Services X 24 Hours',
+      'Daycare Day/ dogs stay 9am to 7pm': 'Daycare Day/ dogs stay 9am to 7pm',
+       'Daycare half day/up to 5 hours': 'Daycare half day/up to 5 hours',
+        'Cat Boarding/ 24 hours': 'Cat Boarding/ 24 hours',
     }
   },
 
   es: {
     translation: {
-      welcome: "¡Bienvenido a Astoria Grooming Holistico para mascotas",
-      "Clean Ears": "Orejas limpias",
-      duration: "Duración",
-      details: "No más detalles",
-      "Extra Desheading": "Extra Desheading",
-      "Face Trim": "Recorte facial",
-      "Cat Claw Trim": "Recorte clássico",
-      "Nail Clipper for dogs": "Recorte de nail",
+      welcome: '¡Bienvenido a Astoria Grooming Holistico para mascotas',
+      //1st row
+        'Cat Claw Trim': 'Adorno de garra de gato',
+        'Nail Clipper for dogs': 'Recorte de nail',
+        'CleanEars': 'Orejas limpias',
+        'Paws Clean': 'Patas limpias',
+
+      //2nd row
+        'Clean Ears': 'Orejas limpias',
+        'Face Trim': 'Recorte facial',
+        'Cats kitty lion cut': 'Gatos gatito león cortado',
+        'Cat Services': 'Servicios para gatos',
+      //3rd row
+        'Cats Long Hair': 'Gatos De Pelo Largo',
+        'Cat classic for Kittens': 'Clásico gato para gatitos',
+        'Extra Dematting': 'Extra Dematting',
+        'Medium double coat': 'Doble capa media',
+
+      //4th row
+        'Full spa extra small dogs': 'Spa completo perros extra pequeños',
+        'Full groom spa small dogs': 'Full groom spa small dogs',
+        'Full groom spa medium dogs':'Spa completo para perros pequeños',
+        'Full groom spa large dogs': 'Spa completo para perros grandes',
       
-      "Cats kitty lion cut": `Este servicio incluye afeitado del cuerpo, mantenimiento de la melena y aseo de los pies y una parte de la cola. También cortamos uñas, limpiamos orejas y ofrecemos un baño seco con un champú especializado con microcristales para limpiar la piel de tu gato. Se pueden aplicar cargos adicionales si el gato tiene el pelaje muy enredado. No afeitamos a gatos mayores de 14 años ni a gatos con bajo peso debido a la fragilidad de su piel y la posibilidad de sufrir lesiones con las maquinillas.
+      //5th row
+        'Full spa extra large dogs': 'Spa completo para perros extra grandes.',
+        'Full spa Double coat Large': 'Spa completo Doble capa Grande',
+        'Dogs basic spa extra small': 'Perros spa básico extra pequeño',
+        'Dogs basic spa small short hair': 'Perros basic spa pequeño pelo corto',
 
-      Para clientes nuevos, requerimos una evaluación inicial para valorar el comportamiento del gato. Si el gato muestra signos de estrés extremo o agresión, es posible que no podamos brindarle toda la gama de servicios. Sin embargo, solo se le cobrarán los servicios que podamos completar. Por ejemplo, si sólo conseguimos cortarle las uñas, sólo se le cobrará por ese servicio.
+      //6th row
+         'Dogs basic spa medium short hair': 'Perros basic spa pelo medio corto',
+         'Dogs basic spa large short hair': 'Perros basic spa grandes pelo corto',
+         'Dogs Basic spa xl dogs': 'Perros basic spa xl',
+         'Dogs Basic small spa double coat': 'Perros Basic pequeño spa doble abrigo',
 
-      No administramos sedantes ni relajantes, ya que eso va en contra de nuestra política. Todas las sesiones de aseo se llevan a cabo en salas separadas para minimizar el estrés, especialmente para los gatos. Se requiere un certificado de rabia para todas las mascotas.`,
-           
-      fullSpaDogs: `Para el ingreso a nuestras instalaciones se requiere un certificado que acredite las vacunas contra la rabia, el moquillo y la bordetella. Su perro disfrutará de un baño relajante con una variedad de champús, que incluyen opciones de avena, sin lágrimas e hipoalergénicos, junto con un acondicionador y un tratamiento facial de arándanos. También cortamos uñas, limpiamos orejas y cortamos pelo. Se aplica un toque final con spray brillante. Es posible que se apliquen tarifas adicionales para perros con pelaje muy enredado.`,
+      //7th row
+        'Dogs Basic small spa long hair': 'Perros Basic pequeño spa pelo largo',
+        'Dogs Basic extra small spa long hair': 'Perros Basic extra pequeño spa pelo largo',
+        'Dogs Basic extra spa double coat': 'Perros Basic spa extra pequeño spa doble capa',
+        'Dogs Basic extra large dog standard poodle and poddle mix spa': 'Perros Spa básico para perros extra grandes, estándar, caniche y poddle mix',
+
+      //8th row
+        'Dogs Basic Extra large long hair': 'Perros Básico Pelo largo extra grande',
+        'Dogs Basic Extra large coat double coat spa': 'Perros Basic Abrigo extra grande doble abrigo spa',
+        'Cocker Spaniel, Cavalier Charles Kings, Cookapoo Or similare mixes': 'Cocker Spaniel, Cavalier Charles Kings, Cookapoo o mezclas similares',
+
+      //9th row
+      'Boarding Services X 24 Hours': 'Servicios de Embarque X 24 Horas',
+      'Daycare Day/ dogs stay 9am to 7pm': 'Guardería/perros de 9:00 a 19:00 horas.',
+      'Daycare half day/up to 5 hours': 'Guardería medio día/hasta 5 horas',
+      'Cat Boarding/ 24 hours': 'Alojamiento para gatos/ 24 horas',
     
-      rabiesCert: `Requerido: Certificado de vacunas al día. Rabia y bordetella`,
-      
-      catsLongHair: `Requisitos: Un certificado de rabia es obligatorio para todos los gatos. Los clientes felinos primerizos necesitarán una evaluación el mismo día de su cita programada. Evaluamos el comportamiento de cada gato y, lamentablemente, no podemos aceptar gatos agresivos o muy estresados. Si el gato sólo nos permite cortarle las uñas, sólo se te cobrará por ese servicio. No se aplicarán cargos si no podemos realizar ningún servicio. Se pueden incurrir en tarifas adicionales para gatos con pelaje muy enredado.`,
-      
-      xtraDmat: "Este servicio es un servicio complementario para perros de doble pelaje.",
-      
-      boarding24: "Durante la noche las 24 horas",
-      
-      catServices: "Recortar uñas, limpiar orejas, zona sanitaria. Requerido: Certificado de vacunas al día. "
-       // ... more keys
+     
     }
   },
 
   el: {
     translation: {
       welcome: 'Καλώς ήρθατε στο Astoria Grooming Holistic Pet Spa',
-      CleanEars: "Orejas limpias",
-      price: "Precio",
-      catLionCut: `Αυτή η υπηρεσία περιλαμβάνει ξύρισμα σώματος, συντήρηση χαίτης και περιποίηση των ποδιών και ενός τμήματος της ουράς. Κόβουμε επίσης νύχια, καθαρίζουμε τα αυτιά και προσφέρουμε στεγνό μπάνιο με εξειδικευμένο σαμπουάν με μικροκρυστάλλους για τον καθαρισμό του δέρματος της γάτας σας. Ενδέχεται να ισχύουν πρόσθετες χρεώσεις εάν η γάτα έχει έντονα μπερδεμένη γούνα. Δεν ξυρίζουμε γάτες ηλικίας άνω των 14 ετών ή λιποβαρείς γάτες λόγω της ευθραυστότητας του δέρματός τους και της πιθανότητας τραυματισμού από την κουρευτική μηχανή.
-
-      Για τους πελάτες για πρώτη φορά, χρειαζόμαστε μια αρχική αξιολόγηση για να αξιολογήσουμε τη συμπεριφορά της γάτας. Εάν η γάτα παρουσιάζει σημάδια ακραίου στρες ή επιθετικότητας, μπορεί να μην είμαστε σε θέση να παρέχουμε όλο το φάσμα των υπηρεσιών. Ωστόσο, θα χρεωθείτε μόνο για τις υπηρεσίες που μπορούμε να ολοκληρώσουμε. Για παράδειγμα, εάν καταφέρουμε να κόψουμε μόνο τα καρφιά, θα χρεωθείτε μόνο για αυτήν την υπηρεσία.
-
-      Δεν χορηγούμε ηρεμιστικά ή χαλαρωτικά, καθώς αυτό αντίκειται στην πολιτική μας. Όλες οι συνεδρίες περιποίησης διεξάγονται σε ξεχωριστούς χώρους για να ελαχιστοποιηθεί το άγχος, ιδιαίτερα για τις γάτες. Απαιτείται Πιστοποιητικό Λύσσας για όλα τα κατοικίδια.`,
       
       // ... more keys
     }
